3fb65340d34c3577222cc5e3d95294e80786d7ea
Sunday Okini <sunihope@gmail.com>
2019-09-18 15:37:21 +0100

    Major changes to files

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let colorCode = document.getElementById("colorcode");
 let color = document.getElementById("color");
-let button = document.getElementById("button");
-button.addEventListener("click", function() {
+let error = document.querySelector('#error');
+
+colorCode.addEventListener("change",displayColor);
+
+function displayColor() {
+  error.textContent="";
   let value1 = colorCode.value;
   if (value1.startsWith("#") && (value1.length === 7 || value1.length === 4)) {
     color.style.backgroundColor = value1;
     }
-  else alert("Input valid color code");
-});
+  else {
+    error.textContent="Specify valid hex color code";
+    error.style.color="#ff0000";
+  }
+}
